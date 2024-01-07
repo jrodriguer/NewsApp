a26c0a48199d5bad2c7b8d94e0f4c165bf082093
Julio <juliorodriguezramirez@gmail.com>
2024-01-07 16:57:41 +0100

    Instance of Custom Tab Bar

---
+++ NewsApp/Views/Custom Tab Bar/CustomTabBar.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabBar.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 7/1/24.
+//
+
+import SwiftUI
+
+struct CustomTabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabBar()
+}
---
+++ NewsApp/Views/RootView.swift
@@ -0,0 +1,22 @@
+//
+//  RootView.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 7/1/24.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Spacer()
+        
+        CustomTabBar()
+    }
+}
+
+#Preview {
+    RootView()
+}
