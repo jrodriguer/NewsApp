0e0ea4aead22041d5bf94fd47864187a2da7cba2
Julio <juliorodriguezramirez@gmail.com>
2024-01-07 01:46:07 +0100

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
