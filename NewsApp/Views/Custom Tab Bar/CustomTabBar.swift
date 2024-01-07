2fcc420bdadb6a8cef3d3e3de67b4d7282048a39
Julio <juliorodriguezramirez@gmail.com>
2024-01-07 16:58:29 +0100

    Differents options for Tab Bar, pre selected icon

--- NewsApp/Views/Custom Tab Bar/CustomTabBar.swift
+++ NewsApp/Views/Custom Tab Bar/CustomTabBar.swift
@@ -9,7 +9,44 @@ import SwiftUI
 
 struct CustomTabBar: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Button {
+                // Switch to Personal
+            } label: {
+                VStack(alignment: .center, spacing: 4) {
+                    Rectangle()
+                        .foregroundColor(.blue)
+                        .frame(width: 59, height: 2)
+                    Image(systemName: "person.crop.circle")
+                        .frame(width: 24, height: 24)
+                    Text("Personal")
+                        //.font(Font.tabBar)
+                }
+            }
+            
+            Button {
+                // Switch to Headers
+            } label: {
+                VStack(alignment: .center, spacing: 4) {
+                    Image(systemName: "network")
+                        .frame(width: 24, height: 24)
+                    Text("Headers")
+                        //.font(Font.tabBar)
+                }
+            }
+            
+            Button {
+                // Switch to Favorites
+            } label: {
+                VStack(alignment: .center, spacing: 4) {
+                    Image(systemName: "heart")
+                        .frame(width: 24, height: 24)
+                    Text("Favorites")
+                        //.font(Font.tabBar)
+                }
+            }
+        }
+        .frame(height: 82)
     }
 }
 
