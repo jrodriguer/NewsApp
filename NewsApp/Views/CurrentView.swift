7b522701733ca881cbb8d54e87370aeee0234ee5
Julio <juliorodriguezramirez@gmail.com>
2024-01-08 01:02:48 +0100

    Style for the custom TabBar item

---
+++ NewsApp/Views/CurrentView.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentView.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 7/1/24.
+//
+
+import SwiftUI
+
+struct CurrentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrentView()
+}
---
+++ NewsApp/Views/CustomTabBar/ShowModalTabBarItem.swift
@@ -0,0 +1,44 @@
+//
+//  ShowModalTabBarItem.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 8/1/24.
+//
+
+import SwiftUI
+
+struct ShowModalTabBarItem: View {
+    let radius: CGFloat
+    let action: () -> Void
+
+    public init(radius: CGFloat, action: @escaping () -> Void) {
+        self.radius = radius
+        self.action = action
+    }
+
+    var body: some View {
+        VStack(spacing: 0) {
+            Image(systemName: "plus.circle.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: radius, height: radius, alignment: .center)
+                .foregroundColor(Color(.systemBlue))
+                .background(Color(.white))
+                .cornerRadius(radius/2)
+                .overlay(RoundedRectangle(cornerRadius: radius/2).stroke(Color(.blue), lineWidth: 2))
+            //Text("Add")
+            //TODO: Add font from .font(Font.tabBar)
+        }
+        .frame(width: radius, height: radius)
+        .onTapGesture(perform: action)
+    }
+}
+
+struct ShowModalTabBarItem_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowModalTabBarItem(radius: 55) {
+            // Action for onTapGesture
+            print("Button tapped")
+        }
+    }
+}
