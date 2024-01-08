e1d2f5af75216c0774b9aa57257288fd1da47d66
Julio <juliorodriguezramirez@gmail.com>
2024-01-08 16:53:50 +0100

    Save the JSON data

---
+++ NewsApp/Bussines/Data/Entities/Article.swift
@@ -0,0 +1,8 @@
+//
+//  Article.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 8/1/24.
+//
+
+import Foundation
---
+++ NewsApp/Preview Content/PreviewData.swift
@@ -0,0 +1,8 @@
+//
+//  PreviewData.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 8/1/24.
+//
+
+import Foundation
