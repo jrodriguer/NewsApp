09d74e7be49cc9feb810cb4f9ae43b5c60a8f675
Julio <juliorodriguezramirez@gmail.com>
2024-01-08 17:40:17 +0100

    Remove file reference

--- NewsApp/Bussines/Services/News.swift
+++ NewsApp/Bussines/Services/News.swift
@@ -14,38 +14,10 @@ class News {
     var categoryAct = ""
     var categoryPg = 0
     
-    func startQuery(query: String) async throws -> TopHeadlines {
-        let urlString = "\(baseUrl)\(query)&apiKey=\(apiKey)"
-        guard let url = URL(string: urlString) else {
-            throw NSError(domain: "Invalid URL", code: 0, userInfo: nil)
-        }
-        
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let httpResponse = response as? HTTPURLResponse,
-              (200...299).contains(httpResponse.statusCode) else {
-            throw NSError(domain: "Server Error", code: 0, userInfo: nil)
-        }
-        
-        let decodedData = try JSONDecoder().decode(TopHeadlines.self, from: data)
-        return decodedData
-    }
+    //TODO: Write startQuery() function
     
-    func getTopHeadLines(completion: @escaping (Result<TopHeadlines, Error>) -> Void) {
-        //TODO: Write getTopHeadLines function
-    }
+    //TODO: Write getTopHeadLines() function
     
-    func getTopHeadLinesCategory(category: String, completion: @escaping (Result<TopHeadlines, Error>) -> Void) {
-        //TODO: Write getTopHeadLinesCategory function
-    }
-}
-
-struct TopHeadlines: Decodable {
-    let articles: [Article]
-}
-
-struct Article: Decodable {
-    let title: String
-    let description: String
-    // TODO: Updaate Article structure
+    //TODO: Write getTopHeadLinesCategory() function
+    
 }
