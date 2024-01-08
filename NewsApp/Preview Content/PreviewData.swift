d8b875f1c1793c1a5873c1860ecd5427896967d0
Julio <juliorodriguezramirez@gmail.com>
2024-01-08 17:46:14 +0100

    Remove unnecessary code

--- NewsApp/Preview Content/PreviewData.swift
+++ NewsApp/Preview Content/PreviewData.swift
@@ -5,4 +5,21 @@
 //  Created by Julio Rodriguez on 8/1/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension JSONDecoder {
+    static var apiDecoder: JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        return decoder
+    }
+}
+
+extension TopHeadlines {
+    static var preview: TopHeadlines {
+        let url = Bundle.main.url(forResource: "TopHeadlines", withExtension: "json")!
+        let data = try! Data(contentsOf: url)
+        let headlines = try! JSONDecoder.apiDecoder.decode(TopHeadlines.self, from: data)
+        return headlines
+    }
+}
