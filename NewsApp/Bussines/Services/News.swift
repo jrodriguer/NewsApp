5d617b5c5f898a6a27caaa6ceccd7c11e3e0293f
Julio <juliorodriguezramirez@gmail.com>
2024-01-08 11:47:51 +0100

    Update Headers view

---
+++ NewsApp/Bussines/Services/News.swift
@@ -0,0 +1,8 @@
+//
+//  News.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 8/1/24.
+//
+
+import Foundation
--- NewsApp/Views/HeadersView.swift
+++ NewsApp/Views/HeadersView.swift
@@ -11,15 +11,9 @@ struct HeadersView: View {
     var body: some View {
         VStack {
             Spacer()
-            HStack {
-                Spacer()
-                Text("Headers")
-                    .font(.system(size: 20))
-                    .bold()
-                Spacer()
+            Grid {
+                
             }
-            
-            Spacer()
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
         .navigationBarTitle("Headers")
