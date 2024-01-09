fcde8d54b223c921f00c5466f7b3b66554a5ecc0
Julio <juliorodriguezramirez@gmail.com>
2024-01-09 12:10:15 +0100

    Update App to create a model instance and using environment

--- NewsApp/NewsAppApp.swift
+++ NewsApp/NewsAppApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct NewsAppApp: App {
+    @State private var modelData = ModelData()
+
     var body: some Scene {
         WindowGroup {
             RootView()
+                .environment(modelData)
         }
     }
 }
