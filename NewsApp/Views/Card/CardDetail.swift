f98772895b3831163a3eff6954c6759fe77c32a2
Julio <juliorodriguezramirez@gmail.com>
2024-01-10 12:21:32 +0100

    Make detail article

---
+++ NewsApp/Views/Card/CardDetail.swift
@@ -0,0 +1,18 @@
+//
+//  CardDetail.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 10/1/24.
+//
+
+import SwiftUI
+
+struct CardDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardDetail()
+}
