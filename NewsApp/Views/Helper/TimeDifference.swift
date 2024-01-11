a8f56b3650d198faf101527f583993fb77c0c3f3
Julio <juliorodriguezramirez@gmail.com>
2024-01-11 17:32:21 +0100

    Add view

---
+++ NewsApp/Views/Helper/TimeDifference.swift
@@ -0,0 +1,23 @@
+//
+//  TimeDifference.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 11/1/24.
+//
+
+import SwiftUI
+
+struct TimeDifference: View {
+    let currentDate = Date()
+    
+    // TODO: Add difference time from article published to today date.
+            
+    var body: some View {
+        Divider()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimeDifference()
+}
