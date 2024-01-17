5c84348b53315f1194d139230172e10ba0eae33f
Julio <juliorodriguezramirez@gmail.com>
2024-01-17 17:22:12 +0100

    Make utils file

---
+++ NewsApp/Utils.swift
@@ -0,0 +1,24 @@
+//
+//  Utils.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 17/1/24.
+//
+
+import Foundation
+
+class Utils {
+    static func displayTitle(_ title: String) -> String {
+        let components = title.components(separatedBy: " - ")
+        return components.first ?? title
+    }
+    
+    static func displayAuthor(_ author: String) -> String {
+        let authors = author.components(separatedBy: ",")
+        if let firstName = authors.first {
+            return firstName.trimmingCharacters(in: .whitespacesAndNewlines) + (authors.count > 1 ? "..." : "")
+        } else {
+            return author
+        }
+    }
+}
