fbf6830b8ab92461c71fd030f0b487b2e3b641c8
Julio <juliorodriguezramirez@gmail.com>
2024-01-25 14:02:28 +0100

    Add swipe to row

--- NewsApp/Views/Articles/ArticleRow.swift
+++ NewsApp/Views/Articles/ArticleRow.swift
@@ -9,14 +9,32 @@ import SwiftUI
 
 struct ArticleRow: View {
     var article: Article
-    
+    @EnvironmentObject var favorites: Favorites
+
     var body: some View {
         HStack {
             Text(Utils.displayTitle(article.title))
+                .swipeActions(edge: .leading) {
+                    Button {
+                        if favorites.contains(article) {
+                            favorites.remove(article)
+                        } else {
+                            favorites.add(article)
+                        }
+                    } label: {
+                        if favorites.contains(article) {
+                            Label("Favorite", systemImage: "heart.slash")
+                        } else {
+                            Label("Favorite", systemImage: "heart.fill")
+                        }
+                    }
+                    .tint(.red)
+                }
         }
     }
 }
 
 #Preview {
-    return ArticleRow(article: ModelData().news.articles[1])
+    ArticleRow(article: ModelData().news.articles[1])
+        .environmentObject(Favorites())
 }
