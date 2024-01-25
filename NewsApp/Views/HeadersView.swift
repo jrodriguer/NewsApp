36585818b61578116882ad267375b79add135dac
Julio <juliorodriguezramirez@gmail.com>
2024-01-25 23:51:50 +0100

    Reindent code

--- NewsApp/Views/HeadersView.swift
+++ NewsApp/Views/HeadersView.swift
@@ -141,7 +141,6 @@ struct HeadersView: View {
                             if article.title != "[Removed]" {
                                 NavigationLink {
                                     ArticleDetail(article: article)
-                                    
                                 } label: {
                                     ArticleRow(article: article)
                                     
