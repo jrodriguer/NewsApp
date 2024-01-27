b1811d3b0c6b20f377687bffdfb5aeade13fcfde
Julio <juliorodriguezramirez@gmail.com>
2024-01-27 14:58:54 +0100

    Add button

--- NewsApp/Views/FloatingActionButton.swift
+++ NewsApp/Views/FloatingActionButton.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct FloatingActionButton: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            //
+        } label: {
+            Image(systemName: "plus")
+                .font(.title2.weight(.bold))
+                .foregroundColor(.white)
+                .padding()
+                .background(.orange)
+                .clipShape(Circle())
+                .shadow(radius: 5, x: 0, y: 3)
+        }
+        .padding()
     }
 }
 
