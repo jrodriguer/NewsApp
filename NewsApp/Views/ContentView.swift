3f0b6c82a6dc7e6c1876afbef98041f9802a60cc
Julio <juliorodriguezramirez@gmail.com>
2024-01-27 14:51:29 +0100

    Refactor tree project

---
+++ NewsApp/Views/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 27/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Environment(ModelData.self) var modelData
+    @StateObject var favorites = Favorites()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                HeadersView(articles: ModelData().news.articles)
+            }
+        }
+        .environmentObject(favorites)
+    }
+}
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
