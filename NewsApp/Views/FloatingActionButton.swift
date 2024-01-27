7097f6780c8fa67cd1e25581d209a9ad91b8c226
Julio <juliorodriguezramirez@gmail.com>
2024-01-27 12:57:30 +0100

    Extranting to separate view

---
+++ NewsApp/Views/FloatingActionButton.swift
@@ -0,0 +1,18 @@
+//
+//  FloatingActionButton.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 27/1/24.
+//
+
+import SwiftUI
+
+struct FloatingActionButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FloatingActionButton()
+}
