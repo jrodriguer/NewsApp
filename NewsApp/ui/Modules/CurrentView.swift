fdeeee6a61690eecbb62f43a9478199582006f3d
Julio <juliorodriguezramirez@gmail.com>
2024-02-09 23:55:29 +0100

    Add current view for specific view based on the currentView

---
+++ NewsApp/ui/Modules/CurrentView.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentView.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 9/2/24.
+//
+
+import SwiftUI
+
+struct CurrentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrentView()
+}
