f70f5a65435249acf9ad0490a624b3e102fdbbb3
Julio <juliorodriguezramirez@gmail.com>
2024-02-09 23:11:59 +0100

    Add old custom tab bar

---
+++ NewsApp/ui/Components/CustomTabBar.swift
@@ -0,0 +1,49 @@
+//
+//  CustomTabBar.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 9/2/24.
+//
+
+import SwiftUI
+
+enum Tabs: Int {
+    case Headers
+    case Personal
+}
+
+// FIXME: TabBar always enable, except into ArticleDetailView.
+
+struct CustomTabBar: View {
+    @Binding var selectedTab: Tabs
+    @Binding var showModal: Bool
+    
+    var body: some View {
+        HStack {
+            Button {
+                selectedTab = .Headers
+            } label: {
+                TabBarButton(buttonText: "Headers", imageName: "network", isActive: selectedTab == .Headers)
+            }
+            .tint(Color(.gray))
+            
+            Button {
+                //
+            } label: {
+                ShowModalTabBarItem(radius: 55) { showModal.toggle() }
+            }
+            
+            Button {
+                selectedTab = .Personal
+            } label: {
+                TabBarButton(buttonText: "Personal", imageName: "face.smiling", isActive: selectedTab == .Personal)
+            }
+            .tint(Color(.gray))
+        }
+        .frame(height: 82)
+    }
+}
+
+#Preview {
+    CustomTabBar(selectedTab: .constant(.Headers), showModal: .constant(false))
+}
---
+++ NewsApp/ui/Components/ShowModalTabBarItem.swift
@@ -0,0 +1,41 @@
+//
+//  ShowModalTabBarItem.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 9/2/24.
+//
+
+import SwiftUI
+
+struct ShowModalTabBarItem: View {
+    let radius: CGFloat
+    let action: () -> Void
+
+    public init(radius: CGFloat, action: @escaping () -> Void) {
+        self.radius = radius
+        self.action = action
+    }
+
+    var body: some View {
+        VStack(spacing: 0) {
+            Image(systemName: "plus.circle.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: radius, height: radius, alignment: .center)
+                .foregroundColor(Color(.systemBlue))
+                .background(Color(.white))
+                .cornerRadius(radius/2)
+                .overlay(RoundedRectangle(cornerRadius: radius/2).stroke(Color(.blue), lineWidth: 2))
+        }
+        .frame(width: radius, height: radius)
+        .onTapGesture(perform: action)
+    }
+}
+
+struct ShowModalTabBarItem_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowModalTabBarItem(radius: 55) {
+            print("Button tapped")
+        }
+    }
+}
