f8151498a4360d434aa780810088c7626dc2d4bd
Julio <juliorodriguezramirez@gmail.com>
2024-02-11 18:12:26 +0100

    Create Color extension

---
+++ NewsApp/ui/Extensions/ColorExtension.swift
@@ -0,0 +1,14 @@
+//
+//  ColorExtension.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 11/2/24.
+//
+
+import SwiftUI
+
+struct ColorExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
