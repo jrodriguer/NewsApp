859e8456f0b5728cf9b10bce76676c99813eb41c
Julio <juliorodriguezramirez@gmail.com>
2024-02-12 19:09:39 +0100

    Remove do - catch code block

--- NewsApp/ui/Modules/Articles/ViewModel/ArticleViewModel.swift
+++ NewsApp/ui/Modules/Articles/ViewModel/ArticleViewModel.swift
@@ -20,53 +20,27 @@ class ArticleViewModel: ObservableObject {
     }
     
     func loadArticles() {
-        do {
-            let list: ArticleListApiObject<ArticleApiObject> = try apiRest.load("TopHeadLines.json")
-            self.articles = list.articles
-        } catch {
-            print("Error loading articles: \(error.localizedDescription)")
-        }
+        let list: ArticleListApiObject<ArticleApiObject> = apiRest.load("TopHeadLines.json")
+        self.articles = list.articles
     }
     
     func loadFavorites() {
-        do {
-            self.articlesIds = try apiRest.get()
-        } catch {
-            print("Error loading favorites: \(error.localizedDescription)")
-        }
+        self.articlesIds = apiRest.get()
     }
     
     func contains(_ article: ArticleApiObject) -> Bool {
         articlesIds.contains(article.id)
     }
     
-    func add(_ article: ArticleApiObject) {
+    func add(_ article: ArticleApiObject) throws {
         objectWillChange.send()
         articlesIds.insert(article.id)
-        do {
-            try apiRest.save(articlesIds)
-        } catch {
-            print("Error adding articles: \(error.localizedDescription)")
-        }
+        apiRest.save(articlesIds)
     }
     
     func remove(_ article: ArticleApiObject) {
         objectWillChange.send()
         articlesIds.remove(article.id)
-        do {
-            try apiRest.save(articlesIds)
-        } catch {
-            print("Error removing articles: \(error.localizedDescription)")
-        }
-    }
-    
-    func swipeFavorite(_ article: ArticleApiObject) {
-        objectWillChange.send()
-        
-        if contains(article) {
-            remove(article)
-        } else {
-            add(article)
-        }
+        apiRest.save(articlesIds)
     }
 }
