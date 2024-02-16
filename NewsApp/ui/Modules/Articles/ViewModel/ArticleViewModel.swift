27b9e7e6b9b0090a83575ce6f811f4a52da2394a
Julio <juliorodriguezramirez@gmail.com>
2024-02-16 13:28:18 +0100

    Temporal fix for Mismatch between model during the decoding process

--- NewsApp/ui/Modules/Articles/ViewModel/ArticleViewModel.swift
+++ NewsApp/ui/Modules/Articles/ViewModel/ArticleViewModel.swift
@@ -19,16 +19,28 @@ class ArticleViewModel: ObservableObject {
     }
     
     func loadArticles() {
-        backendApi?.getArticles()?.responseDecodable(of: [ArticleApiObject].self) { [weak self] response in
+        //FIXME: Mismatch between the expected Swift type during the decoding process.
+        //FIXME: Change 'responseJSON' deprecated to 'responseDecodable'.
+        
+        backendApi?.getArticles()?.responseJSON { [weak self] response in
             guard let self = self else { return }
             switch response.result {
-            case .success(let articlesApObject):
-                self.articles = articlesApObject
-            case .failure(let error):
-                print("Error: \(String(describing: error))")
-                if let data = response.data, let responseString = String(data: data, encoding: .utf8) {
-                    print("Raw response: \(responseString)")
+            case .success(let value):
+                if let dictionary = value as? [String: Any],
+                   let articlesApObject = dictionary["articles"] as? [[String: Any]] {
+                    do {
+                        let jsonData = try JSONSerialization.data(withJSONObject: articlesApObject)
+                        let articles = try JSONDecoder().decode([ArticleApiObject].self, from: jsonData)
+                        self.articles = articles
+                    } catch {
+                        print("Error decoding articles: \(error)")
+                    }
+                } else {
+                    print("Unexpected response format")
                 }
+                
+            case .failure(let error):
+                print("Error: \(error)")
             }
         }
     }
