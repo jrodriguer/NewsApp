d9539b4b4ef1d7bab494137f6bb1556ccf018d28
Julio <juliorodriguezramirez@gmail.com>
2024-02-18 19:41:31 +0100

    Change background color

--- NewsApp/ui/Components/FloatingActionButtonView.swift
+++ NewsApp/ui/Components/FloatingActionButtonView.swift
@@ -19,7 +19,7 @@ struct FloatingActionButtonView: View {
                 .font(.title2.weight(.bold))
                 .foregroundColor(.white)
                 .padding()
-                .background(Color(.detail1))
+                .background(Color(.baseGray))
                 .clipShape(Circle())
                 .shadow(radius: 5, x: 0, y: 3)
         }
