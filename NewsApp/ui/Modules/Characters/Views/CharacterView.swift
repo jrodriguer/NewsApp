f7232f3926f754674b9626c52d8ff1dc8b025da5
Julio <juliorodriguezramirez@gmail.com>
2024-02-20 17:20:45 +0100

    Copy Navigation link from articles

--- NewsApp/ui/Modules/Characters/Views/CharacterView.swift
+++ NewsApp/ui/Modules/Characters/Views/CharacterView.swift
@@ -9,9 +9,23 @@
 
 import SwiftUI
 
+struct Message: Identifiable, Codable {
+    let id: Int
+    var user: String
+    var text: String
+}
+
+enum SearchScope: String, CaseIterable {
+    case inbox, favorites
+}
+
 struct CharacterView: View {
     @StateObject var vm = CharacterViewModel()
+    
+    @State private var messages = [Message]()
+    
     @State private var searchText = ""
+    @State private var searchScope = SearchScope.inbox
 
     var body: some View {
         NavigationStack {
@@ -20,12 +34,13 @@ struct CharacterView: View {
                     ForEach(vm.characters.filter { character in
                         searchText.isEmpty || character.name.contains(searchText)
                     }) { character in
-                        Button(action: {
-                            //
-                        }) {
-                            VStack(alignment: .leading) {
-                                Text(character.name)
+                        ZStack(alignment: .leading) {
+                            Text(character.name)
+                            NavigationLink(destination: Text(character.name)
+                            ) {
+                                EmptyView()
                             }
+                            .opacity(0.0)
                         }
                     }
                 }
@@ -36,6 +51,35 @@ struct CharacterView: View {
             }
         }
         .searchable(text: $searchText)
+        .searchScopes($searchScope) {
+            ForEach(SearchScope.allCases, id: \.self) { scope in
+                Text(scope.rawValue.capitalized)
+            }
+        }
+        .onAppear(perform: runSearch)
+        .onSubmit(of: .search, runSearch)
+        .onChange(of: searchScope) { runSearch() }
+    }
+    
+    var filteredMessages: [Message] {
+        if searchText.isEmpty {
+            return messages
+        } else {
+            return messages.filter { $0.text.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+    
+    // TODO: Add my owner logic.
+    
+    func runSearch() {
+        Task {
+            print("Raw value: \(searchScope.rawValue)")
+
+            guard let url = URL(string: "https://hws.dev/\(searchScope.rawValue).json") else { return }
+            
+            let (data, _) = try await URLSession.shared.data(from: url)
+            messages = try JSONDecoder().decode([Message].self, from: data)
+        }
     }
 }
 
