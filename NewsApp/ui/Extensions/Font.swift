beb16d911273bc6fe7dbd031ab0a12df509f8c98
Julio Rodriguez <juliorodriguezramirez@gmail.com>
2024-02-21 23:48:53 +0100

    Add font

---
+++ NewsApp/ui/Extensions/Font.swift
@@ -0,0 +1,18 @@
+//
+//  Font.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 21/2/24.
+//
+
+import SwiftUI
+
+struct Font: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Font()
+}
