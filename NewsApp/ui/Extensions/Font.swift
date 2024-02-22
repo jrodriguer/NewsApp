0d01958384e246f828dcac92068ac30c924db5f2
Julio Rodriguez <juliorodriguezramirez@gmail.com>
2024-02-22 09:03:56 +0100

    Extension Font

--- NewsApp/ui/Extensions/Font.swift
+++ NewsApp/ui/Extensions/Font.swift
@@ -7,12 +7,8 @@
 
 import SwiftUI
 
-struct Font: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+extension Font {
+    static func customCalligraffitti(size: CGFloat) -> Font {
+        return Font.custom("RegularGraffitti", size: size)
     }
-}
-
-#Preview {
-    Font()
 }
