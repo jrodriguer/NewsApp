6e07f43b21bb7cb4c728eb0edf299e02836be931
Julio <juliorodriguezramirez@gmail.com>
2024-02-23 20:15:45 +0100

    Add an AppDelegate to App

---
+++ NewsApp/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 23/2/24.
+//
+
+import Foundation
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Your code here")
+        return true
+    }
+}
--- NewsApp/NewsApp.swift
+++ NewsApp/NewsApp.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 @main
 struct NewsApp: App {
+    
+    // MARK: Use the UIApplicationDelegateAdaptor property wrapper to tell SwiftUI it should use your AppDelegate class.
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             HomeView()
