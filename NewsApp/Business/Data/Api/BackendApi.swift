2dc5b024fdec4a71b7300888a386708ad86bb3db
jrodriguer <juliorodriguezramirez@gmail.com>
2024-02-23 18:22:56 +0100

    Remove favorites track from Api and

--- NewsApp/Business/Data/Api/BackendApi.swift
+++ NewsApp/Business/Data/Api/BackendApi.swift
@@ -15,8 +15,6 @@ protocol BackendApiProtocol {
 }
 
 class BackendApi: ApiRestManager, BackendApiProtocol {
-    private let saveKey = "Favorites"
-
     let apiUrl: String
     
     init(apiUrl: String) {
@@ -26,7 +24,6 @@ class BackendApi: ApiRestManager, BackendApiProtocol {
     
     func getArticles() -> DataRequest? {
         // TODO: Update way for get query params: apiKey, country.
-        
         let apiKey = "978764b3fe6b412f8517a7d9c0a1e140"
         let serviceURL = "/v2/top-headlines/?country=us&apiKey=\(apiKey)"
         return get(service: serviceURL)
@@ -47,21 +44,6 @@ class BackendApi: ApiRestManager, BackendApiProtocol {
         return get(service: serviceURL)
     }
     
-    // TODO: User.default manager, not API in here.
     // TODO: Check saveKey, param (enum -> articles or characters).
     
-    func saveFavorite(_ id: Set<UUID>) {
-        if let encoded = try? JSONEncoder().encode(id) {
-            UserDefaults.standard.set(encoded, forKey: saveKey)
-        }
-    }
-    
-    func getFavorites() -> Set<UUID> {
-        if let savedIDs = UserDefaults.standard.data(forKey: saveKey),
-           let decodedIDs = try? JSONDecoder().decode(Set<UUID>.self, from: savedIDs) {
-            return decodedIDs
-        } else {
-            return []
-        }
-    }
 }
