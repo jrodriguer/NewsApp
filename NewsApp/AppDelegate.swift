0241921b621d43dcc56df085f0dd7059c3419eff
Julio <juliorodriguezramirez@gmail.com>
2024-02-23 21:13:27 +0100

    Change print string

--- NewsApp/AppDelegate.swift
+++ NewsApp/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        print("Your code here")
+        print("Application Delegate :)")
         return true
     }
 }
