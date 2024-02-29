096a24f83825b779b6134079ef68227be9b38423
Julio <juliorodriguezramirez@gmail.com>
2024-03-01 00:15:45 +0100

    Change to mange an array

--- NewsApp/ui/Modules/Favorites/ViewModel/FavoritesViewModel.swift
+++ NewsApp/ui/Modules/Favorites/ViewModel/FavoritesViewModel.swift
@@ -15,47 +15,41 @@ class FavoritesViewModel<T: Identifiable & Codable>: ObservableObject {
     
     //!! TODO: Refactor to array of favorites.
     
-    private var favorite: T?
+    private var favorites: [T]? = []
     
-    func loadFavorite(_ saveKey: FavoriteKey) -> T? {
-        if let storedItem = UserDefaultsManager<T>.getItem(saveKey),
-           let decodedItem = try? JSONDecoder().decode(T.self, from: storedItem) {
-            favorite = decodedItem
-            return favorite
+    func loadFavorite(_ saveKey: FavoriteKey) -> [T]? {
+        if let storedItems = UserDefaultsManager<[T]>.getItem(saveKey),
+           let decodedItems = try? JSONDecoder().decode([T].self, from: storedItems) {
+            favorites = decodedItems
+            return favorites
         } else {
             return nil
         }
     }
     
     func contains(_ value: T) -> Bool {
-        guard let favorite = favorite else { return false }
-        return favorite.id == value.id
+        guard let favorites = favorites else { return false }
+        return favorites.contains { $0.id == value.id }
     }
     
     func add(_ saveKey: FavoriteKey, value: T) {
         objectWillChange.send()
         
-        favorite = value
-        do {
+        favorites?.append(value)
+        /*do {
             let encoded = try JSONEncoder().encode(favorite)
             UserDefaultsManager<T>.saveItem(saveKey, encoded)
         } catch {
             print("Error encoding value \(error)")
-        }
+        }*/
     }
     
     func remove(_ saveKey: FavoriteKey, value: T) {
         objectWillChange.send()
-        /*do {
-            let encoded = try JSONEncoder().encode(value)
-            UserDefaultsManager<T>.removeItem(saveKey, encoded)
-        } catch {
-            print("Error encoding value \(error)")
-        }*/
         
-        // MARK: Directly delete the object saved in UserDefaults.
-        UserDefaultsManager<T>.removeItem(saveKey)
-        favorite = nil
+        // Directly delete the object saved in UserDefaults.
+        /*UserDefaultsManager<T>.removeItem(saveKey)
+        favorites = nil*/
     }
     
     func filtered(from allItems: [T], showFavoritesOnly: Bool) -> [T] {
