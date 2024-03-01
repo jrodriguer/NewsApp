2815f99e8a69ba1ae9a740e75140ca6b747bf871
Julio <juliorodriguezramirez@gmail.com>
2024-03-01 23:08:28 +0100

    Remove comments

--- NewsApp/Business/Data/Api/PrintProtocol.swift
+++ NewsApp/Business/Data/Api/PrintProtocol.swift
@@ -7,12 +7,9 @@
 
 import Foundation
 
-/// A custom protocol for logging outgoing requests.
 final class PrintProtocol: URLProtocol {
-    
     override public class func canInit(with request: URLRequest) -> Bool {
         print("? Running request: \(request.httpMethod ?? "") - \(request.url?.absoluteString ?? "")")
-        // By returning `false`, this URLProtocol will do nothing less than logging.
         return false
     }
 }
