8109276b5399bbf8816c0762dbe683535797743b
jrodriguer <juliorodriguezramirez@gmail.com>
2024-03-02 15:20:33 +0100

    Add new case test

--- NewsAppTests/Business/Data/Api/BackendApiTests.swift
+++ NewsAppTests/Business/Data/Api/BackendApiTests.swift
@@ -13,11 +13,20 @@ import XCTest
 
 class BackendApiTests: XCTestCase {
     
-    func testGetArticles_Tests() {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func test_BackendApiTests_article_shouldReturnExpectedArticle() {
         let sessionManager = MockGenerator.createMockSessionManager()
         
         let apiKey = "978764b3fe6b412f8517a7d9c0a1e140"
         let apiEndpoint = URL(string: "https://newsapi.org/v2/top-headlines/?country=us&apiKey=\(apiKey)")!
+        
         let expectedArticles = MockGenerator.articleListApiObject()
         let requestExpectation = expectation(description: "Request should finish")
                 
@@ -27,6 +36,26 @@ class BackendApiTests: XCTestCase {
         sessionManager
                 .request(apiEndpoint)
                 .responseDecodable(of: ArticleListApiObject.self) { (response) in
+                    XCTAssertNil(response.error)
+                    requestExpectation.fulfill()
+                }.resume()
+
+            wait(for: [requestExpectation], timeout: 10.0)
+    }
+    
+    func test_BackendApiTests_character_shouldReturnExpectedCharacter() {
+        let sessionManager = MockGenerator.createMockSessionManager()
+        
+        let apiEndpoint = URL(string: "https://rickandmortyapi.com/api/character")!
+        let expectedCharacters = MockGenerator.characterListApiObject()
+        let requestExpectation = expectation(description: "Request should finish")
+                
+        let mock = Mock(url: apiEndpoint, contentType: .json, statusCode: 200, data: [.get: MockGenerator.mockedData(for: expectedCharacters)])
+        mock.register()
+        
+        sessionManager
+                .request(apiEndpoint)
+                .responseDecodable(of: CharacterListApiObject.self) { (response) in
                     XCTAssertNil(response.error)
                     requestExpectation.fulfill()
                 }.resume()
