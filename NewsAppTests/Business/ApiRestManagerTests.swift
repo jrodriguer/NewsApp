ddbf54dae7d02d8d62c86e07be611267d1c61a61
jrodriguer <juliorodriguezramirez@gmail.com>
2024-03-02 14:21:17 +0100

    Move bussines testing files

---
+++ NewsAppTests/Business/ApiRestManagerTests.swift
@@ -0,0 +1,24 @@
+//
+//  ApiRestManagerTests.swift
+//  NewsAppTests
+//
+//  Created by Julio Rodriguez on 28/2/24.
+//
+
+import Foundation
+import XCTest
+@testable import NewsApp
+
+class ApiRestManagerTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+}
