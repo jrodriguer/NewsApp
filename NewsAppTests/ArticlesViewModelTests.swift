adff1995a3e5de74df98cbb4a0cf62d8c84c77f3
jrodriguer <juliorodriguezramirez@gmail.com>
2024-03-03 17:32:56 +0100

    Testing View models (Wip)

--- NewsAppTests/ArticlesViewModelTests.swift
+++ NewsAppTests/ArticlesViewModelTests.swift
@@ -7,18 +7,76 @@
 
 import Foundation
 import XCTest
+import Mocker
 
 @testable import NewsApp
 class ArticlesViewModel_Tests: XCTestCase {
-    
+    var vm = ArticlesViewModel()
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let mockApi = BackendApi(apiUrl: "https://mocked.api")
+        vm = ArticlesViewModel(backendApi: mockApi)
     }
     
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func test_ArticlesViewModel_articles_shouldReturnExpectedArticles() {
+    func testIsLoading_ShouldBeTrue() {
+        XCTAssertTrue(vm.isLoading)
+    }
+    
+    func testLoadArticles_SuccessfulResponse_ShouldUpdateArticles() {
+        let apiKey = "978764b3fe6b412f8517a7d9c0a1e140"
+        let apiEndpoint = URL(string: "https://newsapi.org/v2/top-headlines/?country=us&apiKey=\(apiKey)")!
+        let expectedArticlesList = MockGenerator.articleListApiObject()
+        let mock = Mock(url: apiEndpoint, contentType: .json, statusCode: 200, data: [.get: MockGenerator.mockedData(for: expectedArticlesList)])
+        mock.register()
+        
+        vm.loadArticles()
+        
+        XCTAssertTrue(vm.isLoading)
+        
+        let requestExpectation = expectation(description: "Loading articles")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            // Check that the isLoading property has been reset to false after the simulated loading time
+            XCTAssertFalse(self.vm.isLoading)
+            
+            // FIXME: [] not equal to XCTAssertEqual failed.
+            // Check that the property of items has been updated correctly.
+            XCTAssertEqual(self.vm.articles, expectedArticlesList.articles)
+            
+            // Meets the expectation.
+            requestExpectation.fulfill()
+        }
+        
+        wait(for: [requestExpectation], timeout: 2.0)
+    }
+    
+    func testLoadArticles_FailureResponse_ShouldNotUpdateArticles() {
+        let apiKey = "apikey"
+        let apiEndpoint = URL(string: "https://newsapi.org/v2/top-headlines/?country=us&apiKey=\(apiKey)")!
+        let mock = Mock(url: apiEndpoint, contentType: .json, statusCode: 500, data: [.get: Data()])
+        mock.register()
+
+        vm.loadArticles()
+        
+        XCTAssertTrue(vm.isLoading)
+        
+        let requestExpectation = expectation(description: "Loading articles")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            // Check that the isLoading property has been reset to false after the simulated loading time
+            XCTAssertFalse(self.vm.isLoading)
+            
+            // Check that the properties of items has not been updated correctly.
+            XCTAssertTrue(self.vm.articles.isEmpty)
+            
+            // Meets the expectation.
+            requestExpectation.fulfill()
+            
+        }
+        
+        wait(for: [requestExpectation], timeout: 2.0)
     }
 }
--- NewsAppTests/CharactersViewModelTests.swift
+++ NewsAppTests/CharactersViewModelTests.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import XCTest
+
+@testable import NewsApp
+class CharactersViewModel_Tests: XCTestCase {
+    var vm = CharacterViewModel()
+
+    override func setUpWithError() throws { }
+    
+    override func tearDownWithError() throws { }
+
+    func testLoadCharacters_SuccessfulResponse_ShouldUpdateCharacters() {
+    }
+}
