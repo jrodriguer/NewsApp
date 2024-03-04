035abde5cf3cb9cf486db122683eb0e0c0482d02
Julio <juliorodriguezramirez@gmail.com>
2024-03-04 10:26:11 +0100

    Way Inject depencies (Wip)

---
+++ NewsApp/Dependecies/Dependencies.swift
@@ -0,0 +1,16 @@
+//
+//  Dependencies.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 4/3/24.
+//
+
+import Foundation
+
+class Dependencies {
+    init() { }
+    
+    private func injectDependencies() {
+        
+    }
+}
--- NewsApp/Dependecies/DependencyInjector.swift
+++ NewsApp/Dependecies/DependencyInjector.swift
@@ -8,18 +8,25 @@
 import SwiftUI
 
 class DependencyInjector {
-    struct DependencyInjector {
-        private static var dependencyList: [String: Any] = [:]
-        
-        static func resolve<T>() -> T? {
-            guard let t = dependencyList[String(describing: T.self)] as? T else {
-                fatalError("No provider register for type: \(T.self)")
-            }
-            return t
+    private static var dependencyList: [String: Any] = [:]
+    
+    static func resolve<T>() -> T? {
+        guard let t = dependencyList[String(describing: T.self)] as? T else {
+            fatalError("No provider register for type: \(T.self)")
         }
-        
-        static func register<T>(dependency: T) {
-            dependencyList[String(describing: T.self)] = dependency
-        }
+        return t
+    }
+    
+    static func register<T>(dependency: T) {
+        dependencyList[String(describing: T.self)] = dependency
+    }
+}
+
+@propertyWrapper struct Inject<T> {
+    var wrappedValue: T?
+    
+    init() {
+        self.wrappedValue = DependencyInjector.resolve()
+        Log.verbose("Injected <- \(String(describing: self.wrappedValue))")
     }
 }
