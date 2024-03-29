990cf6be0508b4c76086f5504ce1abb558284288
Julio <juliorodriguezramirez@gmail.com>
2024-03-29 17:44:54 -0300

    Capitalize categories

--- NewsApp/ui/Modules/Articles/Views/ArticleView.swift
+++ NewsApp/ui/Modules/Articles/Views/ArticleView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 enum Category: String, CaseIterable {
-    case business = "business"
-    case entertainment = "entertainment"
-    case general = "general"
-    case health = "health"
-    case science = "science"
-    case sports = "sports"
-    case technology = "technology"
+    case business = "Business"
+    case entertainment = "Entertainment"
+    case general = "General"
+    case health = "Health"
+    case science = "Science"
+    case sports = "Sports"
+    case technology = "Technology"
 }
 
 enum ViewOption: String, CaseIterable {
