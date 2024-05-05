613badf507fbdbbdcf4ca7fc1635985c1f9cc80e
Julio <46267103+jrodriguer@users.noreply.github.com>
2024-05-05 16:44:35 +0200

    feature/Widget_&_Live_Activities - Add SiriKit Intent definition. - Add category model.

---
+++ NewsApp/Business/Data/Entities/Category.swift
@@ -0,0 +1,52 @@
+//
+//  Category.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 5/5/24.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable, Codable {
+    case general
+    case business
+    case technology
+    case entertainment
+    case sports
+    case science
+    case health
+    
+    var text: String {
+        if self == .general {
+            return "Top Headlines"
+        }
+        return rawValue.capitalized
+    }
+    
+    var systemImage: String {
+        switch self {
+        case .general:
+            return "newspaper"
+        case .business:
+            return "building.2"
+        case .technology:
+            return "desktopcomputer"
+        case .entertainment:
+            return "tv"
+        case .sports:
+            return "sportscourt"
+        case .science:
+            return "wave.3.right"
+        case .health:
+            return "cross"
+        }
+    }
+    
+    var sortIndex: Int {
+        Self.allCases.firstIndex(of: self) ?? 0
+    }
+}
+
+extension Category: Identifiable {
+    var id: Self { self }
+}
