27e8e871fd51c2bd23a4b9e4cfb40eb8a31405fd
Julio <46267103+jrodriguer@users.noreply.github.com>
2024-05-06 11:23:39 +0200

    feature/Widget_&_Live_Activities - More use cases for model

--- NewsWidgetShared/Models/ArticleWidgetModel.swift
+++ NewsWidgetShared/Models/ArticleWidgetModel.swift
@@ -23,4 +23,55 @@ struct ArticleWidgetModel: Identifiable {
             return article.id
         }
     }
+    
+    var title: String {
+        switch state {
+        case .placeholder:
+            return "This is a placeholder text for News App"
+        case .article(let article, _):
+            return article.title
+        }
+    }
+    
+    var subtitle: String {
+        switch state {
+        case .placeholder:
+            return "This is a placeholder text for News App"
+        case .article(let article, _):
+            return article.descriptionField
+        }
+    }
+    
+    var url: URL {
+        switch state {
+        case .placeholder:
+            return URL(string: "xcanewsapp://home")!
+        case .article(let article, _):
+            return article.articleURL
+        }
+    }
+    
+    var isPlaceholder: Bool {
+        if case .placeholder = state {
+            return true
+        }
+        
+        return false
+    }
+    
+    var article: ArticleApiObject? {
+        if case .article(let article, _) = state {
+            return article
+        }
+        
+        return nil
+    }
+    
+    var imageData: Data? {
+        if case .article(_, let imageData) = state {
+            return imageData
+        }
+        
+        return nil
+    }
 }
