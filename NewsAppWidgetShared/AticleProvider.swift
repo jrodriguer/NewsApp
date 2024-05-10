531da5e5674659a07d4856bbe5876b76ba553035
Julio <46267103+jrodriguer@users.noreply.github.com>
2024-05-11 01:29:57 +0200

    feature/Widget_&_Live_Activities - Make Provider (Wip)

--- NewsAppWidgetShared/AticleProvider.swift
+++ NewsAppWidgetShared/AticleProvider.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import WidgetKit
+
+struct ArticleProvider: IntentTimelineProvider {
+    typealias Entry = ArticleEntry
+    typealias Intent = SelectCategoryIntent
+    
+    func placeholder(in context: Context) -> ArticleEntry {
+        .placeholder
+    }
+    
+    func getSnapshot(for configuration: SelectCategoryIntent, in context: Context, completion: @escaping (ArticleEntry) -> Void) {
+        // Provide a placeholder for the snapshot
+
+    }
+    
+    func getTimeline(for configuration: SelectCategoryIntent, in context: Context, completion: @escaping (Timeline<ArticleEntry>) -> Void) {
+        let apiUrl = "your_api_url_here"
+        let backendApi = BackendApi(apiUrl: apiUrl)
+        
+    }
+}
