16c61ba8c398812b68e814ffea42192a8f0ccadd
Julio <46267103+jrodriguer@users.noreply.github.com>
2024-05-29 17:07:49 +0200

    main - Create enum for Category

---
+++ NewsApp/Business/Data/Local/Category.swift
@@ -0,0 +1,29 @@
+//
+//  Category.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 29/5/24.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable {
+    case general
+    case business
+    case technology
+    case entertainment
+    case sports
+    case science
+    case health
+    
+    var text: String {
+        if self == .general {
+            return "Top Headlines"
+        }
+        return rawValue.capitalized
+    }
+}
+
+extension Category: Identifiable {
+    var id: Self { self }
+}
