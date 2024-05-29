c6b82bb4721341f3ac7d576be8c14fb55de3dd77
Julio <46267103+jrodriguer@users.noreply.github.com>
2024-05-29 12:58:33 +0200

    main - Remove unnecessary line code

--- NewsAppTests/ArticlesViewModelTests.swift
+++ NewsAppTests/ArticlesViewModelTests.swift
@@ -16,7 +16,6 @@ class ArticlesViewModel_Tests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        vm = ArticlesViewModel()
     }
     
     override func tearDownWithError() throws {
