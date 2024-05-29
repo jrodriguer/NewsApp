aba35829bba801d64c881ecd3ed42cd3612320ac
Julio <46267103+jrodriguer@users.noreply.github.com>
2024-05-29 18:22:10 +0200

    main - Add local mock

--- NewsApp/Business/Data/Local/Entities/Article.swift
+++ NewsApp/Business/Data/Local/Entities/Article.swift
@@ -34,3 +34,18 @@ struct Article: Identifiable {
         )
     }
 }
+
+#if DEBUG
+var MockArticle: Article {
+    return Article(id: UUID(),
+                   author: "Andy Kalmowitz",
+                   title: "Naked Man Running Through Aisle Forces Plane To Land - Jalopnik",
+                   description: "The Virgin Australia flight had to turn back to its original airport because of the unruly passenger.",
+                   url: URL(string: "https://jalopnik.com/naked-man-running-through-aisle-forces-plane-to-land-1851503381")!,
+                   urlToImage: URL(string: "https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/c70ee309b56142b33858105fa544e03e.jpg")!,
+                   publishedAt: Date(),
+                   content: "A man on a Virgin Australia flight from Perth to Melbourne decided this venue was the perfect time to get naked and run down the aisle of the plane, knocking a crew member over in the process. This i… [+1172 chars]",
+                   source: ArticleSource(name: "Jalopnik")
+    )
+}
+#endif
