b15574ead9bb50b9b10699ae572cd59c7002fafb
Julio <46267103+jrodriguer@users.noreply.github.com>
2024-06-17 17:57:37 +0200

    16-functionality-available-to-siri - Implement the Intent Handling (wip). - Register Intent Handler in AppDelegate.

--- NewsApp/AppDelegate.swift
+++ NewsApp/AppDelegate.swift
@@ -7,13 +7,26 @@
 
 import Foundation
 import UIKit
+import Intents
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         print("Application Delegate.\n")
         
         _ = Dependencies()
-        
+            
+        INPreferences.requestSiriAuthorization { status in
+            if status == .authorized {
+                print("Siri authorization granted.")
+                let intent = FetchLatestNewsIntent()
+                let interaction = INInteraction(intent: intent, response: nil)
+                interaction.donate { error in
+                    if let error = error {
+                        print("Failed to donate interaction: \(error.localizedDescription)")
+                    }
+                }
+            }
+        }
         return true
     }
 }
--- Shared/FetchLatestNewsIntentHandler.swift
+++ Shared/FetchLatestNewsIntentHandler.swift
@@ -5,4 +5,20 @@
 //  Created by Julio Rodriguez on 17/6/24.
 //
 
-import Foundation
+import CoreLocation
+import Intents
+
+class FetchLatestNewsIntentHandler: NSObject, FetchLatestNewsIntentHandling {
+
+    func resolveCategory(for intent: FetchLatestNewsIntent, with completion: @escaping (CategoryTypeResolutionResult) -> Void) {
+        if intent.category == .unknown {
+            completion(CategoryTypeResolutionResult.needsValue())
+        } else {
+            completion(CategoryTypeResolutionResult.success(with: intent.category))
+        }
+    }
+    
+    func handle(intent: FetchLatestNewsIntent, completion: @escaping (FetchLatestNewsIntentResponse) -> Void) {
+        let category = intent.category
+    }
+}
