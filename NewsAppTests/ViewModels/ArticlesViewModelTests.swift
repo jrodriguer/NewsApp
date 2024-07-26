e24fbacda215b22e10a298459f789463bb792b10
Julio <46267103+jrodriguer@users.noreply.github.com>
2024-07-26 12:06:03 +0200

    main - Simulates a failure response from the API. - Ensures that the isLoading flag is properly reset and the articles list remains nil.

--- NewsAppTests/ViewModels/ArticlesViewModelTests.swift
+++ NewsAppTests/ViewModels/ArticlesViewModelTests.swift
@@ -25,7 +25,7 @@ class ArticlesViewModelTests: XCTestCase {
         vm = nil
         try super.tearDownWithError()
     }
-        
+    
     func testIsLoading_shouldBeTrue() {
         XCTAssertTrue(vm.isLoading)
     }
@@ -45,7 +45,6 @@ class ArticlesViewModelTests: XCTestCase {
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             // Check that the isLoading property has been reset to false after the simulated loading time
             XCTAssertFalse(self.vm.isLoading)
-                        
             // Meets the expectation.
             requestExpectation.fulfill()
         }
@@ -53,5 +52,28 @@ class ArticlesViewModelTests: XCTestCase {
         wait(for: [requestExpectation], timeout: 2.0)
         
         XCTAssertNotNil(self.vm.articles)
+    }
+    
+    func testLoadArticles_failureResponse_shouldResetLoading() {
+        let apiKey = "978764b3fe6b412f8517a7d9c0a1e140"
+        let apiEndpoint = URL(string: "https://newsapi.org/v2/top-headlines/?country=us&apiKey=\(apiKey)")!
+        let mock = Mock(url: apiEndpoint, contentType: .json, statusCode: 500, data: [.get: Data()])
+        mock.register()
+        
+        vm.loadArticles()
+        
+        XCTAssertTrue(vm.isLoading)
+        
+        let requestExpectation = XCTestExpectation(description: "Loading articles")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            // Check that the isLoading property has been reset to false after the simulated loading time
+            XCTAssertFalse(self.vm.isLoading)
+            // Meets the expectation.
+            requestExpectation.fulfill()
+        }
+        
+        wait(for: [requestExpectation], timeout: 2.0)
+        
+        XCTAssertNil(self.vm.articles)
     }
 }
