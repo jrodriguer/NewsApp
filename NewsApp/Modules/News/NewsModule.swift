db9b2e1dd27c55e017b784289767b78600b20ae8
Julio <46267103+jrodriguer@users.noreply.github.com>
2024-08-22 17:45:51 +0200

    feature/19-reading-the-clean-architecture-with-mvvm - Refactor tree project. - Add News parent module.

---
+++ NewsApp/Modules/News/NewsModule.swift
@@ -0,0 +1,14 @@
+//
+//  NewsModule.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 22/8/24.
+//
+
+import Foundation
+
+final class NewsModule {
+    
+//     TODO: Following hierarchy layers:
+//    View -> ViewModel (Presenter)-> Use Case (Domain) -> DI -> Repository (Data)
+}
