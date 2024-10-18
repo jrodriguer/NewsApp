3b0551d4f9b7f78bcc7a38c7b0d02b2a77c37db4
Julio <46267103+jrodriguer@users.noreply.github.com>
2024-10-18 17:09:49 +0200

    19-reading-the-clean-architecture-with-mvvm - Document method

--- NewsApp/Networking/DataTransferService.swift
+++ NewsApp/Networking/DataTransferService.swift
@@ -19,11 +19,17 @@ final class DefaultDataTransferService: DataTransferService {
         self.networkManager = networkManager
     }
     
+    /// Method to fetch data from Network Manager and Decode the data using decode method
+    /// - Parameter request:  Network request
+    /// - Returns: Decodable type object
     func request<T>(request: any NetworkRequest) async throws -> T where T : Decodable {
         let data = try await networkManager.fetch(request: request)
         return try decode(data: data)
     }
     
+    /// Method to decode data using JSONDecoder
+    /// - Parameter data: Data
+    /// - Returns: Decodable type object
     func decode<T>(data: Data) throws -> T where T : Decodable {
         do {
             let decodedData = try JSONDecoder().decode(T.self, from: data)
