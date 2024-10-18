9285c21c564a878a3c6d86832afc5c13da9047f3
Julio <46267103+jrodriguer@users.noreply.github.com>
2024-10-18 02:07:00 +0200

    - Remove unnecessary file. - Roll back code.

--- NewsApp/Modules/News/Data/Repository/ArticleListRepository.swift
+++ NewsApp/Modules/News/Data/Repository/ArticleListRepository.swift
@@ -7,29 +7,16 @@
 
 import Foundation
 
-protocol ArticleListLocalStorage {
-    func saveArticlesList(_ articles: [ArticleDomainListDTO])
-    func getArticlesList() -> [ArticleDomainListDTO]?
-    func removeArticles(articleIDs: [UUID])
-}
-
 final class DefaultArticleListRepository: ArticleListRepository {
     
     private let service: ArticleListService
-    private let localStorage: ArticleListLocalStorage
     
-    init(service: ArticleListService, localStorage: ArticleListLocalStorage) {
+    init(service: ArticleListService) {
         self.service = service
-        self.localStorage = localStorage
     }
     
     func fetchArticleList() async throws -> [ArticleDomainListDTO] {
-        if let articles = localStorage.getArticlesList() {
-            return articles
-        } else {
-            let fetchedArticles = try await service.fetchArticleListFromNetwork().articles.map{ $0.toDomain() }
-            localStorage.saveArticlesList(fetchedArticles)
-            return fetchedArticles
-        }
+        let fetchedArticles = try await service.fetchArticleListFromNetwork().articles.map{ $0.toDomain() }
+        return fetchedArticles
     }
 }
