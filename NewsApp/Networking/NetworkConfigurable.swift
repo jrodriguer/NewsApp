b0f3ce29e31bf4037b1eecfc174311e2afc051df
Julio <46267103+jrodriguer@users.noreply.github.com>
2024-10-18 20:59:18 +0200

    19-reading-the-clean-architecture-with-mvvm - Fix for host url component

--- NewsApp/Networking/NetworkConfigurable.swift
+++ NewsApp/Networking/NetworkConfigurable.swift
@@ -8,16 +8,16 @@
 import Foundation
 
 protocol NetworkConfigurable {
-    var baseURL: URL { get }
+    var baseURL: String { get }
     var headers: [String: String] { get }
 }
 
 class ApiDataNetworkConfig: NetworkConfigurable {
     
-    let baseURL: URL
+    let baseURL: String
     let headers: [String: String]
     
-     init(baseURL: URL,
+     init(baseURL: String,
         headers: [String: String] = [:]) {
         self.baseURL = baseURL
         self.headers = headers
