8ecb080a934f98f65b3f1c337531c2d7fa7d168d
Julio <46267103+jrodriguer@users.noreply.github.com>
2025-02-26 10:45:40 +0100

    New tab view

---
+++ NewsApp/Modules/Articles/Presentation/Views/SettingsTabView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsTabView.swift
+//  NewsApp
+//
+//  Created by Julio Rodriguez on 26/2/25.
+//
+
+import SwiftUI
+
+struct SettingsTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsTabView()
+}
