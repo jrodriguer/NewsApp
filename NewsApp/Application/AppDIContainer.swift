f49139f357ee015bc7a33e3cfa01b30e21b0b6a2
Julio <46267103+jrodriguer@users.noreply.github.com>
2025-06-07 18:32:34 +0200

    Refactor tree project with performance Dependency Injection

--- NewsApp/Application/AppDIContainer.swift
+++ NewsApp/Application/AppDIContainer.swift
@@ -9,17 +9,34 @@
 
 import Foundation
 
-final class AppDIContainer {
+class AppDIContainer {
+    // Data sources
+    lazy var articleRemoteDataSource = ArticleRemoteDataSource()
+    lazy var articleLocalDataSource = ArticleLocalDataSource()
+    lazy var searchLocalDataSource = SearchHistoryLocalDataSource()
     
-    lazy var apiDataTransferService: DataTransferService = {
-        let networkConfig = ApiDataNetworkConfig(baseURL: AppConfiguration.baseURL)
-        let sessionManager = DefaultNetworkSessionManager(session: SharedURLSession.shared)
-        let networkManager = DefaultNetworkManager(networkConfig: networkConfig, sessionManager: sessionManager)
-        return DefaultDataTransferService(networkManager: networkManager)
-    }()
+    // Repositories
+    lazy var articleListRepositoryProtocol: ArticleListRepositoryProtocol = ArticleListRepository(
+        remoteDataSource: articleRemoteDataSource,
+        localDataSource: articleLocalDataSource
+    )
+    lazy var searchHistoryRepositoryProtocol: SearchHistoryRepositoryProtocol = SearchHistoryRepository(
+        localDataSource: searchLocalDataSource
+    )
     
-    lazy var tabsView: ContentView = {
-        let articlesModule = ArticlesModule(apiDataTransferService: apiDataTransferService)
-        return articlesModule.generateContentView()
-    }()
+    // Use cases
+    lazy var getArticlesUseCase: GetArticleUseCase = GetArticleUseCase(
+        repository: articleListRepositoryProtocol
+    )
+    lazy var manageSearchHistoryUseCase: ManageSearchHistoryUseCase = ManageSearchHistoryUseCase(
+        repository: searchHistoryRepositoryProtocol
+    )
+    
+    // View models
+    func makeArticleViewModel() -> ArticleViewModel {
+        ArticleViewModel(
+            getArticleUseCase: getArticlesUseCase,
+            searchHistoryUseCase: manageSearchHistoryUseCase
+        )
+    }
 }
